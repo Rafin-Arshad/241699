55ed0a3a38d9817e18cb0cef838423494b3360aa
Web App Develop - .Net <hridoy1109024@gmail.com>
2019-05-23 09:14:08 +0600

    Array_file_added

---
+++ MyWinArrayApp/MyWinArrayApp/ArrayOperation.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MyWinArrayApp
+{
+    partial class ArrayOperation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(13, 58);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(580, 305);
+            this.showRichTextBox.TabIndex = 0;
+            this.showRichTextBox.Text = "";
+            // 
+            // ArrayOperation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showRichTextBox);
+            this.Name = "ArrayOperation";
+            this.Text = "ArrayOperation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+    }
+}
---
+++ MyWinArrayApp/MyWinArrayApp/ArrayOperation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWinArrayApp
+{
+    public partial class ArrayOperation : Form
+    {
+        public ArrayOperation()
+        {
+            InitializeComponent();
+            const int size = 10;
+            int []number=new int [size];
+            number[0] = 9;
+            number[5] = 10;
+            number[8] = 15;
+            string message = "";
+            for(int index=0;index<size;index++)
+       
+            {
+                if(number[index]!=0)
+                {
+                    message = message + "Value at the index :" + index + " is= " + number[index]+"\n";
+                }
+                
+            }
+            showRichTextBox.Text = message;
+
+        }
+    }
+}
